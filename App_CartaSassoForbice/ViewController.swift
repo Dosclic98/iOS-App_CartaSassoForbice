303d08b8904315737a327e8f934eff9a37de9438
Davide Savarro <dosclic98@Davides-iMac.local>
2020-02-18 17:47:19 +0100

    Inizio progetto

---
+++ App_CartaSassoForbice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App_CartaSassoForbice
+//
+//  Created by Davide Savarro on 18/02/2020.
+//  Copyright © 2020 Davide Savarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
